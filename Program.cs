460f201220fe1fdcb22a35d115c6ef14080cfa2c
sheryl acree <sacree@tql.com>
2024-06-24 22:30:12 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace EF_Database_First;
+
+internal class Program {
+    static void Main(string[] args) {
+
+        var _context = new TqlprsdbContext();
+        var newRequestLine1 = new Requestline {
+            Id = 0, Quantity = 1, RequestId = 0, ProductId = 2
+        };
+        var newRequestline1 = new Requestline {
+            Id = 0, Quantity = 1, RequestId = 3, ProductId = 2
+        };
+
+        var newRequestline2 = new Requestline {
+            Id = 0, Quantity = 2, RequestId = 2, ProductId = 5
+        };
+       
+       
+
+    
+    
+        _context.SaveChanges();
+     //       var deleteUser = _context.Users.Find(4);
+    //    _context.Users.Remove(deleteUser!);
+     //   _context.SaveChanges();
+     //CONTINUE TO PLAY AROUND WITH ADDING DATA AND WRITE QUERRIES FOR JOINING AND OTHER QUERIES
+
+
+
+        
+
+    }
+}
